6a9a59f91b149eb0baae41360965cd1207054c77
zach <200319235@student.georgainc.on.ca>
2022-10-05 15:16:05 -0400

    Lab 2 upload

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
-# sysinfo.sh made to display  system information
+# sysinfo.sh mark ii made to display  system information
+
+#Title
+echo "./sysinfo.sh 
+
+Report for MyVM
+==============="
+
+#find the fqdn and  print it.  must be assigned to a variable before it can be printed
 FQDN="$(hostname --fqdn)"
 echo "FQDN: " $FQDN
-echo "Host Information:"
-	hostnamectl
-echo "IP address information: "
-ip a |grep -w inet
-ip a |grep -w inet6
-echo "Root Filesystem Status: "
-df /root
+
+#display host operating system
+hostnamectl |grep -w Operating
+
+#find and display IP address
+IPA="$(hostname --all-ip-addresses | awk '{print $1}')"
+echo "IP address:" $IPA
+
+#display available space on root drive
+RS="$(df /root -h|grep -m1 /dev | awk '{print $4}')"
+echo "Root filesystem Free Space: " $RS
+
+#stylize with ending line
+echo "==============="
+
